9327df51e07b06a1218e63f1a587323fab2caceb
akeyi2018 <akeyi2016@gmail.com>
2024-02-20 23:16:16 +0900

    戦闘画面の改良

--- popup_test.py
+++ popup_test.py
@@ -10,7 +10,7 @@ from kivy.uix.label import Label
 from kivy.properties import StringProperty, ListProperty
 from kivy.uix.boxlayout import BoxLayout
 
-kivy.require('2.2.0')
+kivy.require('2.3.0')
 
 Config.set('graphics', 'width', '600')
 Config.set('graphics', 'height', '500')
@@ -19,21 +19,27 @@ Builder.load_file('popup.kv')
 
 from kivy.uix.image import Image
 
+class CustomLayout(BoxLayout):
+    pass
+
 class PopupD(Popup):
     labels = ListProperty()
     images = ListProperty()
+    image_paths = ListProperty()  #  インスタンス変数としてimage_pathsを追加
 
-    def __init__(self, title, labels, images, **kwargs):
+    def __init__(self, title, labels, image_paths, **kwargs):
         super(PopupD, self).__init__(**kwargs)
         self.title = title
         self.labels = labels
-        self.images = images
+        self.images = image_paths  
+        # self.title_bar = None
 
     def on_open(self):
         for text, source in zip(self.labels, self.images):
-            inner_layout = BoxLayout(orientation='vertical')
+            inner_layout = BoxLayout(
+                orientation='vertical', height=150)
             
-            label = Label(text=text)
+            label = Label(text=text, height=100)
             inner_layout.add_widget(label)
             
             image = Image(source=source)
@@ -41,6 +47,33 @@ class PopupD(Popup):
 
             self.ids.content.add_widget(inner_layout)
 
+    def del_monster(self):
+        # モンスターを削除
+        # モンスターが存在する場合
+        if len(self.ids.content.children) > 0:
+            self.ids.content.remove_widget(self.ids.content.children[0])
+    
+    def reconstruct_battle_message(self, new_message):
+        current_message = self.ids.battle_message.text
+        lines = current_message.split('\n')
+        if len(lines) >  5:
+            #  5行を超えた場合、最初の行を削除
+            lines = lines[1:]
+        self.ids.battle_message.text = '\n'.join(lines) + new_message
+
+    def physical_attack(self):
+        #   ここに物理攻撃時の処理を記述します。
+        print("Physical attack executed!")
+        new_message = 'Physical attack executed. \n'
+        self.reconstruct_battle_message(new_message)
+        
+    def magical_attack(self):
+        #   ここに魔法攻撃時の処理を記述します。
+        print("Magical attack executed!")
+        # self.replace_image(1, './enemy_image/black.png', 'New Label Text')
+        new_message = 'Monster defeated. \n'
+        self.del_monster()
+        self.reconstruct_battle_message(new_message)  
 
 class MainDisp(Widget):
 
@@ -57,14 +90,7 @@ class MainDisp(Widget):
                            'wizard',
                            'Snake Woman'
                         ]
-        # image_paths = [
-        #     './enemy_image/enemy_01.gif',
-        #     './enemy_image/enemy_02.gif'
-        #     ]
-        # enemy_name_list = ['goblin',
-        #                    'wizard'
-        #                 ]
-        self.popup = PopupD('battle screen', enemy_name_list, image_paths)
+        self.popup = PopupD('', enemy_name_list, image_paths)
         self.popup.open()
 
 class MainDispApp(App):
